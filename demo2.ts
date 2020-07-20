edaeeb578e298c6162345d8b561ddb17f457cd5c
Shao-Chenlei <shao.chenlei@microfocus.com>
2020-07-20 17:19:05 +0800

    second lesson

---
+++ demo2.ts
@@ -0,0 +1,19 @@
+//类定义重写继承super重新调用(super)
+class Person {
+    name='dell';
+    getName() {
+        return this.name;
+    }
+}
+
+class Teacher extends Person {
+    getTeacherName() {
+        return 'Teacher';
+    }
+    getName() {
+        return super.getName() + 'lee';
+    }
+}
+const teacher = new Teacher();
+console.log(teacher.getName());
+console.log(teacher.getTeacherName())
